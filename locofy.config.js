c9bdb95f7f06e545b88f947bebe7cafc6ec03455
thai-locofy <thai@locofy.ai>
2024-05-06 12:17:02 +0700

    feat: update locofy config

--- locofy.config.js
+++ locofy.config.js
@@ -3,7 +3,8 @@ module.exports = {
     "./src/components/Component.tsx",
     "./src/components/Component1.tsx",
     "./src/components/Component2.tsx",
-    "./src/components/Component3.tsx",
     "./src/components/ComponentRoot.tsx",
+    "./src/components/Header.tsx",
+    "./src/components/Footer.tsx",
   ],
 };
