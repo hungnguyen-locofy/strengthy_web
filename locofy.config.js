9fa8447f454a6504da08805aeae7055e6825b2f8
hungnguyen-locofy <141014699+hungnguyen-locofy@users.noreply.github.com>
2024-05-08 09:26:24 +0700

    Update locofy.config.js

--- locofy.config.js
+++ locofy.config.js
@@ -2,5 +2,15 @@ module.exports = {
   components: [
     "./src/components/Component.tsx",
     "./src/components/TypeComponent.tsx",
+    "./src/components/Card.tsx",
+    "./src/components/Card1.tsx",
+    "./src/components/Card2.tsx",
+    "./src/components/Card3.tsx",
+    "./src/components/Card4.tsx",
+    "./src/components/Component1.tsx",
+    "./src/components/Component2.tsx",
+    "./src/components/ComponentRoot.tsx",
+    "./src/components/Footer.tsx",
+    "./src/components/Header.tsx",
   ],
 };
